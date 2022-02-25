4a21c9c3b813bd6a9c0f8c66c268b3bbe0aa1bc0
Gavin Youker <youkergav@gmail.com>
2022-02-25 18:29:50 -0500

    Added dragging and dropping dice

--- Assets/Combat/UI/_Scripts/UICombatDiceSlot.cs
+++ Assets/Combat/UI/_Scripts/UICombatDiceSlot.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UICombatDiceSlot : MonoBehaviour {
+public class UICombatDiceSlot : MonoBehaviour, IDropHandler {
     public RectTransform DieTemplate;
     [HideInInspector] public string DieUUID;
     private Image DisplayImage;
+
+    public void OnDrop(PointerEventData eventData) {
+        if (eventData.pointerDrag == null) {
+            return;
+        }
+
+        RectTransform die = eventData.pointerDrag.GetComponent<RectTransform>();
+        eventData.pointerDrag.GetComponent<DragDrop>().OnTarget = true;
+
+
+        if (gameObject.transform.Find("Die")) {
+            die.localPosition = Vector2.zero;
+            return;
+        }
+
+        die.SetParent(transform, false);
+        die.localPosition = Vector2.zero;
+    }
 
     public void Set(Die die) {
         if (die == null) {
