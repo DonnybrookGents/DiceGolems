0d3a8d9e9907e8205e3326cf2589e6d78c96cb71
Nate-Perkins <52758095+Nate-Perkins@users.noreply.github.com>
2022-02-25 04:15:59 -0500

    Fixed weak healing on overflow

--- Assets/Combat/Movesets/StatusEffects/ActionFilters/_OverrideExecutions/WeakOverride.cs
+++ Assets/Combat/Movesets/StatusEffects/ActionFilters/_OverrideExecutions/WeakOverride.cs
@@ -6,6 +6,6 @@ public class WeakOverride : ActionFilterOverride {
     public override System.Object Execute(System.Object obj, ActionFilter filter) {
         int damage = (int)obj;
 
-        return damage - filter.Efficacy;
+        return System.Math.Max(damage - filter.Efficacy, 0);
     }
 }
