6fc8998111f863628f518694cb53b440b5bb8ba6
Nate-Perkins <52758095+Nate-Perkins@users.noreply.github.com>
2022-02-26 17:04:48 -0500

    Overworld progression tracking

--- Assets/Game/_Scripts/GameStateController.cs
+++ Assets/Game/_Scripts/GameStateController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public enum GameState { PreLoad, Splash, MainMenu, PauseMenu, Combat, Overworld, GameOver }
 public class GameStateController : MonoBehaviour {
+
+    public static readonly string TAG = "GameController";
     public GameState State;
     public bool _IsStateReady;
 
